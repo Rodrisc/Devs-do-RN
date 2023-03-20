bbe490da7887f3f1ebd61b1bbb642a7df831986a
Rodrigo Soares <87383983+Rodrisc@users.noreply.github.com>
2023-03-19 21:33:20 -0300

    tela inicial e sua estilização

---
+++ views/index/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Devs do RN</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <h1>Dashboard</h1>
+
+    <a href="/views/mostrarAssociados.php">visualizar todos os associados</a>
+    <a href="../cadastroAssociado/cadastro.php">Cadastrar associado</a>
+    <a href="../cadastroAnuidade/ValoreAnuidade.php">Cadastrar anuidade</a>
+    <a href="../alterarAnuidade/alterarAnuidade.php">Alterar valor anuidade</a>
+    <a href="../verificarDevedor/verificarDevedor.php">Verificar pagador e devedor</a>
+    
+</body>
+</html>
